7409140ab0df32495698d1e6452daa8e172c7fa0
charumathir <charumathir@buddi.ai>
2025-04-16 22:09:16 +0530

    Add route and its function to perform withdraw,deposit and balance check

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,34 +1,50 @@
 package controllers
 
+import models.RequestModel
+
 import javax.inject._
 import play.api._
+import play.api.libs.json.Format.GenericFormat
+import play.api.libs.json.{JsSuccess, Json}
 import play.api.mvc._
+import services.AccountServices
+import scala.concurrent.{ExecutionContext, Future}
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject()(val controllerComponents: ControllerComponents)(implicit execution: ExecutionContext) extends BaseController with Logging {
+  val accountService = new AccountServices()
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+
+  def handleRequest(actionName: String)(serviceCall: RequestModel => Future[Result])
+                   (implicit request: Request[AnyContent]): Future[Result] = {
+    request.body.asJson match {
+      case Some(json) =>
+        json.validate[RequestModel] match {
+          case JsSuccess(value, _) =>
+            serviceCall(value)
+          case _ =>
+            logger.error(s"Missing  credentials for $actionName")
+            Future.successful(BadRequest(Json.parse(s"""{ "Error": "Missing credentials to $actionName" }""")))
+        }
+      case None =>
+        logger.error(s"No input provided for $actionName")
+        Future.successful(BadRequest(Json.parse(s"""{ "Error": "Please provide credentials to $actionName" }""")))
+    }
   }
 
-  def withdraw(userId:String,passcode:String,amount:Double)={
-    ???
+  def withdraw(): Action[AnyContent] = Action.async { implicit request =>
+    handleRequest("withdraw")(accountService.withdraw)
   }
-  def deposit()={
-    ???
+
+  def deposit(): Action[AnyContent] = Action.async { implicit request =>
+    handleRequest("deposit")(accountService.deposit)
   }
-  def balanceEnquiry()={
-    ???
+
+  def balanceCheck(): Action[AnyContent] = Action.async { implicit request =>
+    handleRequest("balance check")(accountService.balanceCheck)
   }
 }
